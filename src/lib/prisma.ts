536fc5df9f491c0eb6d536a8c5665ee364bc8682
atharvamhaske <atharvamhaske76@gmail.com>
2025-09-15 20:07:05 +0530

    added email verified and dashboard pages

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@/src/generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
+// Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+// Initialize Prisma Client
+const prisma = globalForPrisma.prisma || 
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  }).$extends(withAccelerate());
 
+// Set the prisma global object in development
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
