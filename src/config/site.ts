d65cd5cadad09d2adc856fc40846b433538976ab
Atharva Mhaske <atharvamhaske76@gmail.com>
2025-09-22 14:58:51 +0530

    fixed build errors

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "BetterAuth Boilerplate",
+  description: "The boilerplate for your next NextJs just use this and iterate over to use this",
+  socials: {
+    github: "https://github.com/yourusername/athubetterauth"
+  }
+}
