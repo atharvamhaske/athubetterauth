23aa34141d2fcdd4fc25e68bae9c1dc768997d87
Atharva Mhaske <atharvamhaske76@gmail.com>
2025-09-22 15:35:45 +0530

    dont need resend as of now

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,18 +1,18 @@
-import {Resend} from "resend"
+// import {Resend} from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-interface SendEmailValues {
-    to: string;
-    subject: string;
-    text: string
-}
+// interface SendEmailValues {
+//     to: string;
+//     subject: string;
+//     text: string
+// }
 
-export async function sendEmail({to, subject, text} : SendEmailValues) {
-    await resend.emails.send({
-        from: "atharva@example.com",
-        to,
-        subject,
-        text
-    });
-}
+// export async function sendEmail({to, subject, text} : SendEmailValues) {
+//     await resend.emails.send({
+//         from: "atharva@example.com",
+//         to,
+//         subject,
+//         text
+//     });
+// }
