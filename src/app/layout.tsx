014d727fb997c8c9dab5dce0797afa8885d08e53
Atharva Mhaske <atharvamhaske76@gmail.com>
2025-09-23 21:29:08 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { satoshi, instrumentSerif } from "./font";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import "./tailwind.css";
 
@@ -28,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${satoshi.variable} ${instrumentSerif.variable}`}
       > 
       {children}
-
+      <Analytics />
       </body>
     </html>
   );
